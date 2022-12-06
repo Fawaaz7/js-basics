e6e03319683d88371925cd840c9bef3d4ca2ab44
Fawaaz Shareef <fawaazshareef@gmail.com>
2022-12-06 09:27:46 +0000

    Create hello.js and index.js

---
+++ hello.js
@@ -0,0 +1,15 @@
+console.log ("hello world");
+// array -list of consecutive elements
+const arr =['78282',23232,undefined,true];
+
+try {
+    console.log(a.university.name);
+} catch (err) {
+    console.log(err);
+}
+
+// arguements
+function square(n) {
+    return n * n;
+}
+console.log(square(2))
